d8efb7a0f270ad89fade58e5abe19040c4ff82f0
Lakisha J <lakishajohnson@pursuit.org>
2021-10-14 13:56:05 -0400

    Added event listener to form

--- main.js
+++ main.js
@@ -22,8 +22,21 @@ fetch("https://ghibliapi.herokuapp.com/films")
         for (let film of films) {
             if(dropDown.value === film.title) {
                 selected = film
-                console.log(film)
+                //console.log(film)
+
+                movie.textContent = film.title;
+                year.textContent = film.release_date;
+                description.textContent = film.description
             }
         }
+    })
+    const form = document.querySelector("form")
+    form.addEventListener("click", (event) => {
+        event.preventDefault();
+        
+        const ul = document.querySelector("ul")
+        const li = document.createElement("li")
+        ul.append(li)
+        li.innerHTML = `<strong><b>${dropDown.value}:</strong></b> ${event.target.review.value}`
     })
 })
