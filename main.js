d4f7a5675b6ff19b124102c732a5f1f9d5c4df61
Lakisha J <lakishajohnson@pursuit.org>
2021-10-14 16:47:09 -0400

    Passing all tests

--- main.js
+++ main.js
@@ -1,47 +1,50 @@
+let dropDown = document.querySelector("#dropDown")
+let movie = document.querySelector("#movie")
+let year = document.querySelector("#year")
+let description = document.querySelector("#description")
+
 fetch("https://ghibliapi.herokuapp.com/films")
 .then((response) => {
     return response.json();
 })
 .then((films) => {
     // console.log(films)
-    let dropDown = document.querySelector("#dropDown")
-
     for(let film of films) {
         const option = document.createElement("option")
         option.setAttribute("value", film.title)
         option.textContent = film.title
         dropDown.append(option)
     }
-    let movie = document.querySelector("#movie")
-    let year = document.querySelector("#year")
-    let description = document.querySelector("#description")
-
     let selected = ""
-
     dropDown.addEventListener("change", () => {
         for (let film of films) {
             if(dropDown.value === film.title) {
                 selected = film
                 //console.log(film)
-
                 movie.textContent = film.title;
                 year.textContent = film.release_date;
                 description.textContent = film.description
             }
         }
     })
-    
-    const form = document.querySelector("#submit-button")
-    const ul = document.querySelector("ul")
-    const input = document.querySelector("form input")
-    
-
+    const form = document.querySelector("#submit")
     form.addEventListener("click", (event) => {
         event.preventDefault();
+    
         const li = document.createElement("li")
-        
+        const input = document.querySelector("form input")
+        const ul = document.querySelector("ul")
         li.innerHTML = `<strong>${dropDown.value}:</strong> ${input.value}` 
         ul.append(li)
-        // document.form.reset()
+        input.value = ""
     })
+    
 })
+
+    
+    
+
+
+
+            
+
